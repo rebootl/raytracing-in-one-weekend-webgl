9882002b2762bb2b61112edf3d44a224bb986ac0
Cem Aydin <cem.aydin@gmx.ch>
2022-02-17 14:27:58 +0100

    chapt. 6.1, normals

--- main2.js
+++ main2.js
@@ -53,13 +53,17 @@ const fs = `
     vec3 direction;
   };
 
-  bool hitSphere(vec3 center, float radius, Ray r) {
+  float hitSphere(vec3 center, float radius, Ray r) {
     vec3 oc = r.origin - center;
     float a = dot(r.direction, r.direction);
     float b = 2.0 * dot(oc, r.direction);
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4.*a*c;
-    return (discriminant > 0.);
+    if (discriminant < 0.) {
+      return -1.0;
+    } else {
+      return (-b - sqrt(discriminant) ) / (2.0*a);
+    }
   }
 
   vec3 at(float t, Ray r) {
@@ -67,10 +71,12 @@ const fs = `
   }
 
   vec3 rayColor(Ray r) {
-    if (hitSphere(vec3(0, 0, -1), 0.5, r)) {
-      return vec3(1, 0, 0);
+    float t = hitSphere(vec3(0, 0, -1), 0.5, r);
+    if (t > 0.) {
+      vec3 N = normalize(at(t, r) - vec3(0, 0, -1));
+      return 0.5 * (N + 1.);
     }
-    float t = 0.5 * (normalize(r.direction).y + 1.);
+    t = 0.5 * (normalize(r.direction).y + 1.);
     return (1. - t) * vec3(1., 1., 1.) + t * vec3(0.5, 0.7, 1.0);
   }
 
