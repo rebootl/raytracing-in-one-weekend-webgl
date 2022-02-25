189d4d86ee3caee80a138342caf20d8f095ff84b
Cem Aydin <cem.aydin@gmx.ch>
2022-02-25 22:43:49 +0100

    example + clear blue working

--- main3.js
+++ main3.js
@@ -78,247 +78,231 @@ const vs = `
 `;
 
 const fs = `
-precision mediump float;
+  // fragment shaders don't have a default precision so we need
+  // to pick one. mediump is a good default
+  precision mediump float;
 
-uniform vec2 canvasDimensions;
-uniform float time;
+  uniform sampler2D inputTex;
+  uniform vec2 canvasDimensions;
+  uniform float time;
 
-//uniform int	PASSINDEX;
-uniform sampler2D inputTex;
-//uniform sampler2D view;
-//uniform sampler2D backbuffer;
-
-#define MAX_FLOAT 1e5
-#define MAX_RECURSION 5
-#define PI 3.1415926535897932385
-#define TAU 2. * PI
-// Φ = Golden Ratio
-#define PHI 1.61803398874989484820459
+  const int NSPHERES = 2;
+  const int MAX_DEPTH = 5;
 
 
-float g_seed = 0.25;
-
-float deg2rad(float deg){
-  return deg*PI / 180.;
-}
-
-// random number generator
-
-//https://stackoverflow.com/a/34276128
-bool isnan(float x){
-  return !(x > 0. || x < 0. || x == 0.);
-}
-
-// a variation of gold noise is used
-// https://stackoverflow.com/a/28095165
-// https://www.shadertoy.com/view/ltB3zD
-// centered around [0-1] in gaussian
-float random (vec2 st) {
-     return fract(tan(distance(st*PHI, st)*g_seed)*st.x);
-}
-
-vec2 random2(float seed){
-  return vec2(
-    random(vec2(seed-1.23, (seed+3.1)* 3.2)),
-    random(vec2(seed+12.678, seed - 5.8324))
-    );
-}
-
-vec3 random3(float seed){
-  return vec3(
-    random(vec2(seed-0.678, seed-0.123)),
-    random(vec2(seed-0.3, seed+0.56)),
-    random(vec2(seed+0.1234, seed-0.523))
-    );
-}
-
-vec3 random_in_unit_sphere( float seed) {
-  vec2 tp = random2(seed);
-  float theta = tp.x * TAU;
-  float phi = tp.y * TAU;
-  vec3 p = vec3(sin(theta) * cos(phi), sin(theta)*sin(phi), cos(theta));
-
-  return normalize(p);
-}
-
-vec3 random_unit(float seed){
-    vec2 rand = random2(seed);
-    float a = rand.x * TAU;
-    float z = (2. * rand.y) - 1.;
-    float r = sqrt(1. - z*z);
-    return vec3(r*cos(a), r*sin(a), z);
-}
+  #define MAX_FLOAT 1e5
+  #define MAX_RECURSION 5
+  #define PI 3.1415926535897932385
+  #define TAU 2. * PI
+  // Φ = Golden Ratio
+  #define PHI 1.61803398874989484820459
 
 
-// ray primitive
-
-struct ray{
-  vec3 o, dir;
-};
-
-vec3 ray_at(ray r, float t){
-  return r.o + r.dir*t;
-}
-
-struct hit{
-  vec3 n,p;
-  float t;
-  bool front_face;
-};
+  float g_seed = 0.25;
 
 
-// camera
+  // random number generator
 
-struct camera{
-  vec3 o, lower_left, horizontal, vertical;
-};
-
-camera make_camera(){
-  float h = 2.0;
-  float aspect = (canvasDimensions.x / canvasDimensions.y);
-  float w = h * aspect;
-
-  vec3 o = vec3(0.,0.,0.);
-  vec3 horizontal = vec3(w, 0,0);
-  vec3 vertical = vec3(0.,h,0.);
-  vec3 lower_left = o - horizontal / 2. - vertical / 2. -
-    vec3(0, 0, 1.);
-  return camera(o, lower_left, horizontal, vertical);
-}
-
-ray camera_get_ray(camera c, vec2 uv){
-  ray r =  ray(c.o,
-    normalize(c.lower_left + uv.x * c.horizontal + uv.y * c.vertical - c.o));
-    return r;
-}
-
-
-// intersections
-
-struct sphere{
-  vec3 center;
-  float radius;
-};
-
-bool hit_sphere(sphere s, ray r, vec2 t_min_max, inout hit rec){
-  vec3 oc = r.o - s.center;
-  float b = dot(oc, r.dir);
-  float c = dot(oc,oc) - s.radius*s.radius;
-  float d = b*b - c;
-  if(d < 0.){ return false; }
-
-  float t1 = (-b - sqrt(d));
-  float t2 = (-b + sqrt(d));
-  float t = t1 < t_min_max.x ? t2 : t1;
-
-  vec3 p = ray_at(r, t);
-  vec3 n = (p - s.center);
-  // if front_face, the ray is in the sphere and not out, so invert normal
-  bool front_face = dot(r.dir, n) > 0.;
-
-  n = front_face ? -n : n;
-  n /= s.radius;
-  if(t < t_min_max.x || t > t_min_max.y){
-    return false;
+  //https://stackoverflow.com/a/34276128
+  bool isnan(float x){
+    return !(x > 0. || x < 0. || x == 0.);
   }
 
-  rec = hit(n,p,t, front_face);
-  return true;
-}
-
-
-// raytracing
-
-bool raycast(const in ray r, vec2 t_min_max, inout hit h){
-  sphere s = sphere(vec3(0,0,-2.), 1.);
-  sphere pl = sphere(vec3(0.,-201, -2.), 200.);
-
-  bool is_hit = false;
-  is_hit = hit_sphere(pl, r, t_min_max, h) || is_hit;
-  is_hit = hit_sphere(s, r, t_min_max, h) || is_hit;
-  return is_hit;
-}
-
-vec3 ray_color(ray r){
-  vec2 t_min_max = vec2(0.001, MAX_FLOAT);
-  hit h;
-  vec3 col = vec3(1.);
-
-// #define debug_normal
-#ifdef debug_normal
-  if(raycast(r, t_min_max, h)){
-    return .5 * (h.n + vec3(1.));
+  // a variation of gold noise is used
+  // https://stackoverflow.com/a/28095165
+  // https://www.shadertoy.com/view/ltB3zD
+  // centered around [0-1] in gaussian
+  float random (vec2 st) {
+       return fract(tan(distance(st*PHI, st)*g_seed)*st.x);
   }
-#else
 
-  for(int i=0; i < MAX_RECURSION; i++){
-    bool is_hit = raycast(r, t_min_max, h);
-    if(is_hit){
-      vec3 jitter = random_unit(g_seed);
-      if(isnan(jitter.r) || isnan(jitter.g) || isnan(jitter.b)){
-        jitter = vec3(0.);
-      }
+  vec2 random2(float seed){
+    return vec2(
+      random(vec2(seed-1.23, (seed+3.1)* 3.2)),
+      random(vec2(seed+12.678, seed - 5.8324))
+      );
+  }
 
-      col *= 0.5;
-      vec3 target = (h.p + h.n + jitter);
-      r.o = h.p;
-      r.dir = normalize(target - h.p);
-    }else{
+  vec3 random3(float seed){
+    return vec3(
+      random(vec2(seed-0.678, seed-0.123)),
+      random(vec2(seed-0.3, seed+0.56)),
+      random(vec2(seed+0.1234, seed-0.523))
+      );
+  }
 
-      vec3 unit_dir = normalize(r.dir);
-      float t = 0.5 * (unit_dir.y + 1.0);
-      col *= mix(vec3(1.0), vec3(0.5,0.7,1.0), t);
-      return col;
+  vec3 random_in_unit_sphere( float seed) {
+    vec2 tp = random2(seed);
+    float theta = tp.x * TAU;
+    float phi = tp.y * TAU;
+    vec3 p = vec3(sin(theta) * cos(phi), sin(theta)*sin(phi), cos(theta));
+
+    return normalize(p);
+  }
+
+  vec3 random_unit(float seed){
+      vec2 rand = random2(seed);
+      float a = rand.x * TAU;
+      float z = (2. * rand.y) - 1.;
+      float r = sqrt(1. - z*z);
+      return vec3(r*cos(a), r*sin(a), z);
+  }
+
+
+  struct Ray {
+    vec3 origin;
+    vec3 direction;
+  };
+
+  struct Sphere {
+    vec3 center;
+    float radius;
+    // .. mat. etc.
+  };
+
+  struct hitRecord {
+    vec3 p;
+    vec3 normal;
+    float t;
+    bool frontFace;
+  };
+
+
+
+  void setFaceNormal(const Ray r, const vec3 outwardNormal, inout hitRecord rec) {
+    rec.frontFace = dot(r.direction, outwardNormal) < 0.;
+    rec.normal = rec.frontFace ? outwardNormal : -outwardNormal;
+  }
+
+  vec3 at(const float t, const Ray r) {
+    return r.origin + t * r.direction;
+  }
+
+  bool hitSphere(const Sphere s, const Ray r, const float tMin,
+      const float tMax, inout hitRecord rec) {
+    vec3 oc = r.origin - s.center;
+    float a = pow(length(r.direction), 2.);
+    float halfB = dot(oc, r.direction);
+    float c = pow(length(oc), 2.) - s.radius * s.radius;
+    float discriminant = halfB * halfB - a * c;
+    if (discriminant < 0.) {
+      return false;
     }
-  }
-  return col;
-#endif
-}
+    float sqrtd = sqrt(discriminant);
 
-void main(){
-  //vec2 p = (gl_FragCoord.xy * 2. - canvasDimensions) / min(canvasDimensions.x, canvasDimensions.y);
-  vec2 uv = gl_FragCoord.xy / canvasDimensions;
-
-  camera c = make_camera();
-  // new seed every frame
-  g_seed = random(gl_FragCoord.xy * (mod(time, 100.)));
-  if(isnan(g_seed)){
-    g_seed = 0.25;
-  }
-
-  // anti aliasing
-  vec2 jitter = (2. * random2(g_seed)) - 1.;
-  vec2 st = uv + jitter * 0.001;
-  // check for NaN leakage
-  if(isnan(st.x) || isnan(st.y)){
-    st = uv;
+    float root = (-halfB - sqrtd) / a;
+    if (root < tMin || tMax < root) {
+      root = (-halfB + sqrtd) / a;
+      if (root < tMin || tMax < root) {
+        return false;
+      }
+    }
+    rec.t = root;
+    rec.p = at(root, r);
+    vec3 outwardNormal = (rec.p - s.center) / s.radius;
+    setFaceNormal(r, outwardNormal, rec);
+    return true;
   }
 
-  // multisampling in veda
-  // we use multipass, which does one pass (0) where we accumulate to a buffer (accum)
-  // the second pass (1) takes the average of the accumulated values
-  //if(PASSINDEX == 0){
-  ray r = camera_get_ray(c, st);
+  bool worldHit(const Sphere[NSPHERES] spheres, const Ray r, const float tMin,
+      const float tMax, out hitRecord rec) {
+    hitRecord tempRec;
+    bool hitAnything = false;
+    float closestSoFar = tMax;
 
-  vec3 col = ray_color(r);
-
-    // approximate gamma correction
-    // col = sqrt(col);
-
-  vec4 result = vec4(col,1.) + texture2D(inputTex, uv);
-
-  if(result.a > MAX_FLOAT){
-    result = vec4(result.xyz/result.a, 1.);
+    for (int i = 0; i < NSPHERES; i++) {
+      if (hitSphere(spheres[i], r, tMin, closestSoFar, tempRec)) {
+        hitAnything = true;
+        closestSoFar = tempRec.t;
+        rec = tempRec;
+      }
+    }
+    return hitAnything;
   }
-  gl_FragColor = result;
 
-/*
-  if(PASSINDEX == 1){
-    vec4 result = texture2D(accum, uv);
+  vec3 rayColor(Ray r, const Sphere[NSPHERES] spheres) {
 
-    gl_FragColor = vec4(result.xyz / result.a, 1.);
-  }*/
-}
+    hitRecord rec;
+    vec3 col = vec3(0.);
+
+    for (int s = 0; s < MAX_DEPTH; s++) {
+
+      bool hit = worldHit(spheres, r, 0.001, 99999., rec);
+
+      if (hit) {
+        vec3 jitter = random_unit(g_seed);
+        if(isnan(jitter.r) || isnan(jitter.g) || isnan(jitter.b)){
+          jitter = vec3(0.);
+        }
+
+        vec3 target = rec.p + rec.normal + jitter;
+        r = Ray(rec.p, normalize(target - rec.p));
+
+        col = 0.5 * col;
+        // (normal)
+        //col = 0.5 * (rec.normal + vec3(1,1,1));
+      } else {
+        // background
+        float t = 0.5 * (normalize(r.direction).y + 1.);
+        col = (1. - t) * vec3(1., 1., 1.) + t * vec3(0.5, 0.7, 1.0);
+        return col;
+      }
+    }
+
+    return col;
+  }
+
+  void main() {
+    vec2 uv = gl_FragCoord.xy / canvasDimensions;
+
+    // new seed every frame
+    g_seed = random(gl_FragCoord.xy * (mod(time, 100.)));
+    if(isnan(g_seed)){
+      g_seed = 0.25;
+    }
+
+    float aspectRatio = canvasDimensions.x / canvasDimensions.y;
+
+    // Camera
+
+    const float viewportHeight = 2.0;
+    float viewportWidth = aspectRatio * viewportHeight;
+    const float focalLength = 1.0;
+
+    const vec3 origin = vec3(0, 0, 0);
+    vec3 horizontal = vec3(viewportWidth, 0, 0);
+    vec3 vertical = vec3(0, viewportHeight, 0);
+    vec3 lowerLeftCorner = origin - horizontal / 2. - vertical / 2.
+      - vec3(0, 0, focalLength);
+
+    Sphere spheres[NSPHERES];
+    spheres[0] = Sphere(vec3(0, 0, -1), 0.5);
+    spheres[1] = Sphere(vec3(0, -100.5, -1), 100.);
+
+
+    //vec2 st = (gl_FragCoord.xy + randomVec2(0., 1.)) / (canvasDimensions - 1.);
+
+    // anti aliasing
+    vec2 jitter = (2. * random2(g_seed)) - 1.;
+    vec2 st = uv + jitter * 0.001;
+    // check for NaN leakage
+    if(isnan(st.x) || isnan(st.y)){
+      st = uv;
+    }
+
+    Ray r = Ray(
+      origin,
+      normalize(lowerLeftCorner + st.x * horizontal + st.y * vertical - origin)
+    );
+    vec3 pixelColor = rayColor(r, spheres);
+
+    vec4 result = vec4(pixelColor,1.) + texture2D(inputTex, uv);
+
+    if(result.a > MAX_FLOAT){
+      result = vec4(result.xyz/result.a, 1.);
+    }
+    gl_FragColor = result;
+  }
 `;
 
 const drawVS = `
@@ -393,7 +377,6 @@ function main() {
   const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
   const canvasDimensionsLocation = gl.getUniformLocation(program, 'canvasDimensions');
   const inputTexLocation = gl.getUniformLocation(program, 'inputTex');
-  //const randTexLocation = gl.getUniformLocation(program, 'randTex');
   const timeLocation = gl.getUniformLocation(program, 'time');
   const niterLocation = gl.getUniformLocation(program, 'niter');
 
@@ -422,10 +405,6 @@ function main() {
     new Array(canvas.width * canvas.height).fill(0)
       .map(() => [0, 0, 0, 0]).flat()
   );
-  /*const texdata2 = new Float32Array(
-    new Array(canvas.width * canvas.height).fill(0)
-      .map(() => [0, 0, 0, 0]).flat()
-  );*/
 
   const tex1 = createTexture(gl, texdata, canvas.width, canvas.height);
   const tex2 = createTexture(gl, texdata, canvas.width, canvas.height);
@@ -450,12 +429,6 @@ function main() {
   // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  /*const texdatarand = new Float32Array(
-    new Array(canvas.width * canvas.height * 4).fill(0)
-      .map(() => [ rand(0, 1), 0, 0, 0 ]).flat()
-  );
-  const texr = createTexture(gl, texdatarand, canvas.width, canvas.height);
-*/
   for (let i = 0; i < niter; i++) {
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, output.FB);
@@ -479,18 +452,13 @@ function main() {
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, input.tex);
 
-    //gl.activeTexture(gl.TEXTURE0 + 1);
-    //gl.bindTexture(gl.TEXTURE_2D, texr);
-
     gl.uniform1i(inputTexLocation, 0);
     gl.uniform2f(canvasDimensionsLocation, gl.canvas.width, gl.canvas.height);
     const t = Date.now() / 1000;
     const i = parseInt((t - parseInt(t)) * 1000);
-    //console.log(i)
     gl.uniform1f(timeLocation, i);
     gl.uniform1f(niterLocation, niter);
 
-    //gl.uniform1i(randTexLocation, 1);
     // draw
     // primitiveType, offset, count
     gl.drawArrays(gl.TRIANGLES, 0, 6);
@@ -531,13 +499,6 @@ function main() {
     const t1 = output;
     output = input;
     input = t1;
-
-    /*const t1 = outputFB1;
-    outputFB1 = outputFB2;
-    outputFB2 = t1;
-    const t2 = outputTex1;
-    outputTex1 = outputTex2;
-    outputTex2 = t2;*/
   }
 }
 
